87c9e1197f3371609c7d3891398f080ddc035615
SHIVA PRASAD SARKAR <132213662+Shiva-Prasad-Sarkar@users.noreply.github.com>
2024-10-14 14:27:10 +0600

    Update Roman to Numeric Conversion.py

--- Roman to Numeric Conversion.py
+++ Roman to Numeric Conversion.py
@@ -1,9 +1,9 @@
-s=input('Enter a roman number: ')
+roman=input('Enter a roman number: ')
 num=0    
 dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-for i in range(len(s)):
-    if i+1<len(s) and dic[s[i]] < dic[s[i+1]]:
-        num -= dic[s[i]]
+for i in range(len(roman)):
+    if i+1<len(roman) and dic[roman[i]] < dic[roman[i+1]]:
+        num -= dic[roman[i]]
     else:
-        num += dic[s[i]]
+        num += dic[roman[i]]
 print(num)        
